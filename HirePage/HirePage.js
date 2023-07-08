a37d30589861d143c93d40f413375c4fdbe37738
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-07-09 04:07:11 +0530

    added transitions to landing page

--- HirePage/HirePage.js
+++ HirePage/HirePage.js
@@ -10,49 +10,84 @@ async function fetchApplicantsData() {
   
   async function initializeApp() {
     const applicantsData = await fetchApplicantsData();
+    let filteredApplicants = [];
   
-    function generateDataboxElements(applicants) {
+    function generateDataboxElements() {
       const candidateDataWrap = document.querySelector('.candidateDataWrap');
+      candidateDataWrap.innerHTML = ''; // Clear existing content
   
-      applicants.forEach(applicant => {
-        const databox = document.createElement('div');
-        databox.classList.add('databox');
+      filteredApplicants.forEach(applicant => {
+        if (!applicant.shortlist) {
+          const databox = document.createElement('div');
+          databox.classList.add('databox');
   
-        const name = document.createElement('div');
-        name.classList.add('item', 'dataName');
-        name.textContent = applicant.name;
+          const name = document.createElement('div');
+          name.classList.add('item', 'dataName');
+          name.textContent = applicant.name;
   
-        const location = document.createElement('div');
-        location.classList.add('item', 'dataLocation');
-        location.textContent = applicant.location;
+          const location = document.createElement('div');
+          location.classList.add('item', 'dataLocation');
+          location.textContent = applicant.location;
   
-        const emptyDiv = document.createElement('div');
-        emptyDiv.classList.add('item');
+          const emptyDiv = document.createElement('div');
+          emptyDiv.classList.add('item');
   
-        const role = document.createElement('div');
-        role.classList.add('item', 'dataRole');
-        role.textContent = applicant.job_role;
+          const role = document.createElement('div');
+          role.classList.add('item', 'dataRole');
+          role.textContent = applicant.job_role;
   
-        const rating = document.createElement('div');
-        rating.classList.add('item', 'dataRating');
-        rating.textContent = applicant.rating;
+          const rating = document.createElement('div');
+          rating.classList.add('item', 'dataRating');
+          rating.textContent = applicant.rating;
   
-        const shortlistBtn = document.createElement('button');
-        shortlistBtn.classList.add('item', 'shortlistbtn');
-        shortlistBtn.textContent = 'Shortlist';
+          const shortlistBtn = document.createElement('button');
+          shortlistBtn.classList.add('item', 'shortlistbtn');
+          shortlistBtn.textContent = 'Shortlist';
+          shortlistBtn.addEventListener('click', () => {
+            applicant.shortlist = true; // Update shortlist value in memory
+            generateDataboxElements(); // Re-render databox elements
+          });
   
-        databox.appendChild(name);
-        databox.appendChild(location);
-        databox.appendChild(emptyDiv);
-        databox.appendChild(role);
-        databox.appendChild(rating);
-        databox.appendChild(shortlistBtn);
+          databox.appendChild(name);
+          databox.appendChild(location);
+          databox.appendChild(emptyDiv);
+          databox.appendChild(role);
+          databox.appendChild(rating);
+          databox.appendChild(shortlistBtn);
   
-        candidateDataWrap.appendChild(databox);
+          candidateDataWrap.appendChild(databox);
+        }
       });
     }
   
-    generateDataboxElements(applicantsData.applicants);
+    function applyFilters() {
+      const searchName = document.querySelector('.searchinp').value.toLowerCase().trim();
+      const searchLocation = document.querySelector('.Locationinp').value.toLowerCase().trim();
+      const searchRole = document.querySelector('.roleinp').value.toLowerCase().trim();
+  
+      filteredApplicants = applicantsData.applicants.filter(applicant => !applicant.shortlist); // Exclude shortlisted applicants
+  
+      if (searchName || searchLocation || searchRole) {
+        filteredApplicants = filteredApplicants.filter(applicant => {
+          const nameMatch = searchName === '' || applicant.name.toLowerCase().includes(searchName);
+          const locationMatch = searchLocation === '' || applicant.location.toLowerCase().includes(searchLocation);
+          const roleMatch = searchRole === '' || applicant.job_role.toLowerCase().includes(searchRole);
+  
+          return nameMatch && locationMatch && roleMatch;
+        });
+      }
+  
+      generateDataboxElements();
+    }
+  
+    const searchInputs = document.querySelectorAll('.searchinp, .Locationinp, .roleinp');
+    searchInputs.forEach(input => {
+      input.addEventListener('input', applyFilters);
+    });
+  
+    applyFilters(); // Initial data load
+  
+    generateDataboxElements();
   }
   
   initializeApp();
---
+++ LandingPage.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition)
+    if (scrollPosition > 1500) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  
+    const textElement = document.querySelector('.lowerheader');
+    const boxd1 = document.querySelector('#boxdesc1');
+    const boxd2 = document.querySelector('#boxdesc2');
+    const boxd3 = document.querySelector('#boxdesc3');
+    const s1img = this.document.querySelector('.setoneimg');
+    const s1para = this.document.querySelector('.setonepara');
+    const s2img = this.document.querySelector('.settwoimg')
+    const s2para = this.document.querySelector('.settwopara')
+    const windowHeight = window.innerHeight;
+    const triggerPosition1 = 500; // Adjust the scroll trigger position as desired
+    const triggerPosition2 = 1000;
+    const triggerPosition3= 1300;
+    
+    if (scrollPosition > triggerPosition1) {
+      textElement.style.opacity='1';
+      textElement.style.transform='translateY(0)';
+    }
+    if(scrollPosition > triggerPosition1){
+        boxd1.style.opacity='1';
+        boxd1.style.transform='translateX(0)'
+        boxd2.style.opacity='1';
+        boxd2.style.transform='translateX(0)'
+        boxd3.style.opacity='1';
+        boxd3.style.transform='translateX(0)'
+    }
+    if(scrollPosition > triggerPosition2){
+        s1img.style.opacity='1';
+        s1para.style.transform='translateX(0)'
+    }
+    if(scrollPosition > triggerPosition3){
+        s2img.style.opacity='1';
+        s2para.style.transform='translateX(0)'
+    }
+  });
+  
